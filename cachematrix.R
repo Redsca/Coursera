c0d3b1709ad597292ccd21c84f8a2c485fe9ebf6
Redsca <redsca@hotmail.com>
2016-10-03 23:57:03 +1100

    Week 3 Assignment
    Week 3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Changed the mean to INV
+## Changed the mean function to Solve
+## Both function write to same location
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  INV <- NULL
+  set <- function(y) {
+    x <<- y
+    INV <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) INV <<- inverse
+  getInverse <- function() INV
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  INV <- x$getInverse()
+  if(!is.null(INV)) {
+    message("getting cached data")
+    return(INV)
+  }
+  Matrx <- x$get()
+  INV <- solve(Matrx, ...)
+  x$setInverse(INV)
+  INV
 }
